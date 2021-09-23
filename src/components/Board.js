4b5908474c9cef8dd4c03b77c0e9de445ed399be
Mac Rowe <m.rowe42@gmail.com>
2021-09-23 15:27:10 -0400

    Made board larger

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,8 @@ import Square from './Square';
 const style = {
     border: '4px solid darkblue',
     borderRadius: '10px', 
-    width: '250px',
-    height: '250px',
+    width: '500px',
+    height: '500px',
     margin: '0 auto',
     display: 'grid', 
     gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)' 
