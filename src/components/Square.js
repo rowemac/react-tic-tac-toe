1cf996c8ad084765474457df6d5a091107d25a9b
Mac Rowe <m.rowe42@gmail.com>
2021-09-23 15:12:04 -0400

    Add styling to Board and Square components

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,16 @@
 import React from 'react'; 
 
-// const props = {
-//     onClick: () => "function",
-//     value: "X"
-// };
-
-// const { value } = props; 
+const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue', 
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+}
 
 const Square = ({ value, onClick }) => (
-    <button onClick={onClick}>
+    <button style={style} onClick={onClick}>
         {value}
     </button>
 )
